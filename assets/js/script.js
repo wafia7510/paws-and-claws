c62dd3dfd411cb42ff58ccebb7600638ac4b746e
Wafia Durrani <wafiadr2@gmail.com>
2024-08-26 23:27:51 +0000

    Add variables for all elements in index file

--- assets/js/script.js
+++ assets/js/script.js
@@ -1,36 +1,29 @@
 let score = 0;
 let health = 0;
-let level = 0;
-let gameDuration = 20000; // 20 seconds per level
 let gameInterval;
-let elementInterval;
-const holeElements=[]
+let interval_speed = 3000;
+let displayTime = 2500;
+let displayTimeout;
+let max_health = 9;
+health=max_health;
+let no_of_level = ["Slow", "Medium", "Fast"];
+let level =no_of_level[0];
+
 /** Elements dom id
  * for score , health , level ,start/restart buttons and holes
 */
+const soundToggleButton = document.getElementById("soundToggleButton");
+const gameSound = document.getElementById("gameSound"); // Get the audio element
 const scoreDisplay = document.getElementById('score');
 const healthDisplay = document.getElementById('health');
 const levelDisplay = document.getElementById('level');
 const startBtn = document.getElementById('startBtn');
 const restartBtn = document.getElementById('restartBtn');
-const holes = document.querySelectorAll('.holes');
-
-function startGame(){
-    clearTimeout(timeClock);// prms has to go in
-    clearInterval(intervalSpeed);
-    let timeClock=setTimeout(showHoles, 3000);
-    let intervalSpeed=setInterval(showHoles,3000);
-}
-function endGame(){}
-function restartGame(){
-}
-function nextLevel(){
-}
-function healthBoost(){}
-function incrementScore(){
-
-}
-function scoreDown(){}
-function whackACat(){}
-function whackADog(){}
-function showHoles(){}
+const holes = document.getElementsByClassName("holes");
+const winnerScreen = document.getElementById("WinnerScreen");
+const gameOverScreen=document.getElementById("gameOverScreen");
+const soundBtn = document.getElementById("soundButton");
+const grid = document.getElementById("grid"); // Get the grid element
+scoreDisplay.textContent=score;
+levelDisplay.textContent=level;
+healthDisplay.textContent=health;
